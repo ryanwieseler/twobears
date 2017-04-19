97b623141fc98af21912797df90cbcbb33f2e8c9
Ryan Wieseler <ryan.wieseler@gmail.com>
2017-04-18 23:24:41 -0700

    added bear logo and tweaked breakpoints.

--- js/twobears.js
+++ js/twobears.js
@@ -1,40 +1,49 @@
 $(document).ready(function() {
-  $("#aboutus-tbg").css("height", $("#about-us").height());
+  $(window).on('load', function() {
+    $("#aboutus-tbg").css("height", $("#about-us").height());
+    $("#engagement-tbg").css("height", $("#our-engagement").height());
+  })
 
-   $(window).resize(function() {
-     var aboutusheight = $("#about-us").height();
-     $("#aboutus-tbg").css("height", aboutusheight);
-   })
+  $(window).resize(function() {
+    var aboutusheight = $("#about-us").height();
+    $("#aboutus-tbg").css("height", aboutusheight);
+    var ourengagement = $("#our-engagement").height();
+    $("#engagement-tbg").css("height", ourengagement);
+  })
 
-   $("#engagement-tbg").css("height", $("#our-engagement").height());
+  $('.nav a').on('click', function(){
+    $(".nav").find(".active").removeClass("active");
+    $(this).parent().addClass("active");
+    $('#navbar').collapse('hide');
+    console.log('hello, wies');
+  });
 
-    $(window).resize(function() {
-      var ourengagement = $("#our-engagement").height();
-      $("#engagement-tbg").css("height", ourengagement);
-    })
+  $(".navbar-nav li a[href^='#'], .pocket a").on('click', function(e) {
+     // prevent default anchor click behavior
+     e.preventDefault();
 
-    $('.nav a').on('click', function(){
-      $(".nav").find(".active").removeClass("active");
-      $(this).parent().addClass("active");
-      $('#navbar').collapse('hide');
-    });
+     // store hash
+     var hash = this.hash;
 
-    $(".navbar-nav li a[href^='#']").on('click', function(e) {
-       // prevent default anchor click behavior
-       e.preventDefault();
+     // animate
+     $('html, body').animate({
+         scrollTop: $(this.hash).offset().top
+       }, 300, function(){
 
-       // store hash
-       var hash = this.hash;
+         // when done, add hash to url
+         // (default click behaviour)
+         window.location.hash = hash;
+       });
+  });
 
-       // animate
-       $('html, body').animate({
-           scrollTop: $(this.hash).offset().top
-         }, 300, function(){
-
-           // when done, add hash to url
-           // (default click behaviour)
-           window.location.hash = hash;
-         });
-     });
-
+  $(document).scroll(function(){
+    var el = $('.bear-icon'),
+        top = $('#our-wedding').offset().top - $(document).scrollTop();
+    if (top < 100){
+        $(el).addClass('in-view');
+    }
+    if (top > 100){
+        $(el).removeClass('in-view');
+    }
+  });
 });
